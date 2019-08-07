a4faabb0a7ef54dc64f06e6bef00e42238fc739e
Harry Wells <harry.wells@insight.com>
2019-08-07 15:08:50 -0400

    SF5.3.34 - Create [a] Spring Framework DI Example Project

---
+++ src/main/java/com/udemy/sf5/didemo/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.sf5.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiDemoApplication.class, args);
+    }
+
+}
